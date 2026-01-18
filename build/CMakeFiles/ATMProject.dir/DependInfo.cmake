63a2c4eeff0849e51121d48d2a5bd9ac52fe6a26
grey-photon <haadi5584@gmail.com>
2026-01-18 19:24:56 +0500

    added built files for the first time

---
+++ build/CMakeFiles/ATMProject.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Haadi/OneDrive - Higher Education Commission/Desktop/LUMS/Assignments etc/CS100 Project/ATM Account Simulator/Group34_ATMAccountSimulator_Rev5.1/src/main.cpp" "CMakeFiles/ATMProject.dir/src/main.cpp.obj" "gcc" "CMakeFiles/ATMProject.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ATMProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ATMProject.exe"
+  "ATMProject.exe.manifest"
+  "ATMProject.pdb"
+  "CMakeFiles/ATMProject.dir/src/main.cpp.obj"
+  "CMakeFiles/ATMProject.dir/src/main.cpp.obj.d"
+  "libATMProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATMProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
